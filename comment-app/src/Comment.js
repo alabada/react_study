e118422cd3f337ef6e213da48b7496c376e2a6f8
wenzhida <zhida.wen@qishon.com>
2017-03-26 13:56:48 +0800

    评论增加时间戳

--- comment-app/src/Comment.js
+++ comment-app/src/Comment.js
@@ -1,6 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes} from 'react'
 
 class Comment extends Component {
+
+  static propTypes = {
+    comment: PropTypes.object.isRequired
+  }
+
+  constructor () {
+    super()
+    this.state = { timeString: '' }
+  }
+
+  componentWillMount () {
+    this._updateTimeString()
+    this._timer = setInterval(
+      this._updateTimeString.bind(this),
+      5000
+    )
+  }
+
+  _updateTimeString () {
+    const comment = this.props.comment
+    const duration = (+Date.now() - comment.createdTime) / 1000
+    this.setState({
+      timeString: duration > 60
+        ? `${Math.round(duration / 60)} 分钟前`
+        : `${Math.round(Math.max(duration, 1))} 秒前`
+    })
+  }
+
 
   render () {
     return (
@@ -9,9 +37,13 @@ class Comment extends Component {
           <span>{this.props.comment.username} </span>：
         </div>
         <p>{this.props.comment.content}</p>
+        <span className='comment-createdtime'>
+          {this.state.timeString}
+        </span>
       </div>
     )
   }
+
 }
 
 export default Comment
