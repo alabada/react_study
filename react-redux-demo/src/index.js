1e13a69c0d2c90bd9b89d533f3ab5690c3902018
zhida.wen <zhida_wen@163.com>
2017-07-20 14:30:48 +0800

    add some

--- react-redux-demo/src/index.js
+++ react-redux-demo/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import Header from './Header'
 import Content from './Content'
 import './index.css'
+import { Provider } from './react-redux'
 
 function createStore (reducer) {
   let state = null
@@ -30,16 +31,7 @@ const themeReducer = (state, action) => {
 }
 
 const store = createStore(themeReducer)
-
 class Index extends Component {
-  static childContextTypes = {
-    store: PropTypes.object
-  }
-
-  getChildContext () {
-    return { store }
-  }
-
   render () {
     return (
       <div>
@@ -50,7 +42,10 @@ class Index extends Component {
   }
 }
 
+// 把 Provider 作为组件树的根节点
 ReactDOM.render(
-  <Index />,
+  <Provider store={store}>
+    <Index />
+  </Provider>,
   document.getElementById('root')
 )
